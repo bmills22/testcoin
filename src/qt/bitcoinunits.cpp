8b334a21092c3ce7243d2ae7ee4a7bc1f549a072
bmills22 <blaakemilla@gmail.com>
2014-08-06 15:22:38 -0700

    Name change

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("testcoins");
+    case mBTC: return QString("Milli-testcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-testcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
